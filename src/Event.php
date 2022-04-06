69ad7a739fd43116b9537d25953854f541db517b
Sergei Zhidkov <zhidkoff@list.ru>
2022-04-06 09:14:07 +0400

    add onCron() method to Event class

--- src/Event.php
+++ src/Event.php
@@ -10,4 +10,17 @@ class Event extends \Illuminate\Console\Scheduling\Event
      * @var string
      */
     public string $cron = 'default';
+
+    /**
+     * Set the desired cron for the job.
+     *
+     * @param string $cron
+     *
+     * @return $this
+     */
+    public function onCron(string $cron): static
+    {
+        $this->cron = $cron;
+        return $this;
+    }
 }
